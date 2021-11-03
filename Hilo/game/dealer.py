75aad12b37f90e149c22f14eac57974e5261fa6e
Joshua Dean <dea16006@byui.edu>
2021-11-03 09:25:21 -0600

    Update dealer.py

--- Hilo/game/dealer.py
+++ Hilo/game/dealer.py
@@ -9,14 +9,16 @@ class Dealer:
         pass
 
     def draw_card(self):
-        ''''''
+        '''A draw card method that pulls a new card'''
         list_of_cards = [1,2,3,4,5,6,7,8,9,10,11, 12, 13]
         return random.choice(list_of_cards)
 
     def get_current_card(self):
+        '''A current card method that. The dealer draws a current card'''
         self.current_card = self.draw_card()
 
     def get_next_card(self):
+        '''A next card method. The dealer draws the next card'''
         self.next_card = self.draw_card()
 
   
