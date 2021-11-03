625974048cfaf94810a700eefb886c146bbf4bb5
Joshua Dean <dea16006@byui.edu>
2021-11-03 09:21:58 -0600

    Update dealer.py

--- Hilo/game/dealer.py
+++ Hilo/game/dealer.py
@@ -3,11 +3,20 @@ import random
 class Dealer:
     def __init__(self):
         '''A constructor method for the Dealer class.'''
+        self.current_card = 0
+        self.next_card = 0
+        self.h_or_l = ""
+        pass
 
-        self.current_card = ""
-        self.next_card = ""
-        self.current_card_val = 0
-        self.next_card_val= 0
-        
+    def draw_card(self):
+        ''''''
+        list_of_cards = [1,2,3,4,5,6,7,8,9,10,11, 12, 13]
+        return random.choice(list_of_cards)
+
+    def get_current_card(self):
+        self.current_card = self.draw_card()
+
+    def get_next_card(self):
+        self.next_card = self.draw_card()
 
   
