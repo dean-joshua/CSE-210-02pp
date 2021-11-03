52cf35fb47b6523452a21aebbb28026f39f648b4
Joshua Dean <dea16006@byui.edu>
2021-11-03 09:10:21 -0600

    Created and added to repository

---
+++ Hilo/game/dealer.py
@@ -0,0 +1,13 @@
+import random
+
+class Dealer:
+    def __init__(self):
+        '''A constructor method for the Dealer class.'''
+
+        self.current_card = ""
+        self.next_card = ""
+        self.current_card_val = 0
+        self.next_card_val= 0
+        
+
+  
---
+++ Hilo/game/director.py
@@ -0,0 +1,24 @@
+from game.dealer import Dealer
+
+class Director:
+    def __init__(self):
+        '''A constructor method for the Director class.'''
+        self.keep_playing = True
+        self.score = 300
+        self.dealer = Dealer()
+
+    def start_game(self):
+        ''' The start_game method for the Director class. Continues the game as long as the user is able to'''
+        while self.keep_playing:
+            self.get_inputs
+            self.do_updates
+            self.do_outputs
+
+    def get_inputs(self):
+        pass
+
+    def do_updates(self):
+        pass
+
+    def do_outputs(self):
+        pass
