3109eb4b68321ca2a5b64780fa28e1ce69ea073b
mashmelow99 <81062236+mashmelow99@users.noreply.github.com>
2021-11-04 09:48:23 -0600

    Update director.py

--- Hilo/game/director.py
+++ Hilo/game/director.py
@@ -10,16 +10,36 @@ class Director:
     def start_game(self):
         ''' The start_game method for the Director class. Continues the game as long as the user is able to'''
         while self.keep_playing:
-            self.get_inputs
-            self.do_updates
-            self.do_outputs
+            self.get_inputs()
+            print(f"The current card is {self.dealer.current_card}")
+            self.dealer.get_h_or_l()
+            print(f"The next card is {self.dealer.next_card}")
+
+            self.do_updates()
+            
+            self.do_outputs()
 
     def get_inputs(self):
-        '''hi'''
-        pass
+
+        self.get_current_card()
+        self.get_next_card()
+        
+
+        
 
     def do_updates(self):
-        pass
+        points = self.dealer.get_points()
+        if points  == 100:
+            self.score += points 
+        elif points  == 75:
+            self.score -= points
+        
 
     def do_outputs(self):
-        pass
+        print(f"Your score is: {self.score}")
+        if self.score > 0:
+            choice = input("Play Again? [y/n] ")
+            self.keep_playing = (choice == "y")
+        else:
+            self.keep_playing = False
+        
