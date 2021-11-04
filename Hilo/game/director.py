00c83319625f1fc89a5d4b14229b2e386be58243
Joshua Dean <dea16006@byui.edu>
2021-11-04 11:05:46 -0600

    Update director methods
    Added separators around the start_game method code to make the console display more readable. Added method descriptions to some of the methods in directory.py

--- Hilo/game/director.py
+++ Hilo/game/director.py
@@ -8,26 +8,25 @@ class Director:
         self.dealer = Dealer()
 
     def start_game(self):
-        ''' The start_game method for the Director class. Continues the game as long as the user is able to'''
+        ''' The start_game method for the Director class. Continues the game as long as the user can still play.'''
         while self.keep_playing:
             self.get_inputs()
+            print("+---+---+---+---+")
             print(f"The current card is {self.dealer.current_card}")
             self.dealer.get_h_or_l()
             print(f"The next card is {self.dealer.next_card}")
-
-            self.do_updates()
-            
+            self.do_updates()           
             self.do_outputs()
+            print("+---+---+---+---+")
+            print("")
 
     def get_inputs(self):
-
+        '''The get inputs method takes in all the necessary input for the game to start.'''
         self.dealer.get_current_card()
-        self.dealer.get_next_card()
-        
-
-        
+        self.dealer.get_next_card()       
 
     def do_updates(self):
+        '''Updates the state of the game. This includes adding points when the user guesses correctly.'''
         points = self.dealer.get_points()
         if points  == 100:
             self.score += points 
@@ -36,6 +35,7 @@ class Director:
         
 
     def do_outputs(self):
+        '''Displays the score and choice to play again to the user.'''
         print(f"Your score is: {self.score}")
         if self.score > 0:
             choice = input("Play Again? [y/n] ")
